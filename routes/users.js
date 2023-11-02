095b7c7bfa0faa98d7f522a25d920ef4f46bb64b
Daksh Kanotra <dakshkanotra@gmail.com>
2023-11-02 16:39:58 +0530

    Added Routes for Users

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const jwt = require('jsonwebtoken')
 const router = express.Router();
-
 const User = require('../models/Client');
+const { SECRET } = require('../utils/config');
 
 router.get('/', async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  const users = await User.findAll({
+    attributes: ['id', 'ip', 'mac', 'age', 'loginTime', 'logoutTime']
+  });
+  res.json(users);
 });
+
+router.get('/me', async (req, res) => {
+  const token = req.token
+  const decoded = jwt.verify(token, SECRET);
+  const userId = decoded.id;
+
+  const user = await User.findByPk(userId, {
+    attributes: { exclude: ['password', 'logoutTime'] }
+  });
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+
+})
 
 module.exports = router;
